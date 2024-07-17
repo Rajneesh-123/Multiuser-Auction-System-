c354a64406fe6fb94585b0b0a26f2a9abc2edbea
Rajneesh Chaudhary <rajneeshchaudhary14102@gmail.com>
2024-07-17 16:40:35 +0530

    Remove files and folders

--- db.php
+++
@@ -1,4 +0,0 @@
-<?php 
-$con = new mysqli('localhost','root','','multiuser-auction-system');
-date_default_timezone_set('asia/kolkata');
- ?>
--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-include('db.php');
-session_start();
-session_destroy();
-header("location:index.php");
-?>
